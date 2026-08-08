71aca2d672ab508840019bcbb093d8a506475ef7
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-08 14:16:49 +0530

    feat: practice scope resolution operator with std::cout

---
+++ input_output/output_with_cout.cpp
@@ -0,0 +1,10 @@
+// To print output, we use cout from the iostream library. Since it belongs to the standard namespace, we write std::cout.
+
+#include<iostream>
+
+int main (){
+
+  std::cout<<"I am Roshan Mallick"; ///--------> :: this is called scope resolution operator 
+  
+ return 0;
+}
