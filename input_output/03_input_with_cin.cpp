375b874a3071632db1b7f2340931558cff8272fe
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-08 16:00:09 +0530

    feat: practice user input with std::cin

---
+++ input_output/03_input_with_cin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    int x;
+
+    std::cout << "Enter x: ";
+    std::cin >> x;  // cin takes user input from the standard namespace
+
+    std::cout << "You have entered: " << x << "\n";
+
+    return 0;
+}
