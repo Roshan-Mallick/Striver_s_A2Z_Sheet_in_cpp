467066be6713607d705212ec1a547b8118a3f718
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-08 16:58:07 +0530

    feat: add program to check data type sizes on current system

---
+++ input_output/data_type_sizes_on_current_system.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "bool: " << sizeof(bool) << " byte(s)\n";
+    std::cout << "char: " << sizeof(char) << " byte(s)\n";
+    std::cout << "int: " << sizeof(int) << " byte(s)\n";
+    std::cout << "float: " << sizeof(float) << " byte(s)\n";
+    std::cout << "double: " << sizeof(double) << " byte(s)\n";
+    std::cout << "long int: " << sizeof(long int) << " byte(s)\n";
+    std::cout << "long long int: " << sizeof(long long int) << " byte(s)\n";
+    return 0;
+}
