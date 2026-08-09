58e6d0dd51f346ee541d005e82c8395eeec4db86
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-09 17:37:38 +0530

    Add variable naming rules in C++ basics

---
+++ 02_cpp_basics/01_variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Variable names are case-sensitive
+    
+    int age;        // lowercase
+    int Age;        // uppercase A
+    int AGE2;       // uppercase letters + digit
+    int age_2;      // underscore between characters
+    int _age_2;     // starts with an underscore
+    
+    // age, Age, and AGE are three different variables
+    // Variable names can contain letters, digits, and underscore (_), ---->
+   // ----> but cannot start with a digit, contain spaces/special characters, or be a C++ keyword.
+
+    return 0;
+}
