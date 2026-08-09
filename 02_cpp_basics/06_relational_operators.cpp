86ddab573f259bda4110b3d82fd0b18607679784
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-09 23:10:32 +0530

    Add relational operators example

---
+++ 02_cpp_basics/06_relational_operators.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+ int a = 10 ;
+ int b = 10 ;
+ int c = 20 ;
+ 
+ cout << "a = " << a << endl;
+ cout << "b = " << b << endl;
+ cout << "c = " << c << endl;
+ 
+ cout << "a == b : " << (a == b) << endl;
+ cout << "a == c : " << (a == c) << endl;
+ cout << "a != c : " << (a != c) << endl;
+ cout << "a != c : " << (a != b) << endl;
+ cout << "a > b  : " << (a > b) << endl;
+ cout << "a < c  : " << (a < c) << endl;
+ cout << "a >= b : " << (a >=b) << endl;
+ cout << "a <= c : " << (a <=c) << endl;
+ cout << "a >= c : " << (a >= c) << endl;
+ 
+
+
+
+ return 0;
+ 
+}
