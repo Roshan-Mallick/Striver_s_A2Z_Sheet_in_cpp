d2b7945009a079aafeea554c479b5f0c8afb383a
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-09 22:28:24 +0530

    Add C++ data types example

---
+++ 02_cpp_basics/03_data_types.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main  () {
+
+  int age = 1177;
+  float height = 6.5f;
+  double pi = 3.14;
+  char grade = 'A';
+  bool is_Student = true;
+  long int population = 1000000;
+  long long int bigNumber = 900000000000000;
+  
+  
+  cout << "Age = "<<age << endl;
+  cout << "Height = " << height << endl;
+  cout << "Pi = " << pi << endl;
+  cout << "Student = "<< is_Student << endl;
+  cout << "Population = "<< population <<endl;
+  cout << "Big Number = "<<bigNumber << endl;
+
+  return 0;
+}
