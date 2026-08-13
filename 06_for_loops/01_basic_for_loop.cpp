334511686f805fb55b8261f71e638dd8aa7e154f
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-13 23:32:50 +0530

    feat: add basic for loop program printing 1 to 10

---
+++ 06_for_loops/01_basic_for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Loop starts at i = 1, runs while i <= 10, and increments i by 1 each time
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+
+    cout << endl; // Good practice: add a newline at the end of output
+
+    return 0;
+}
