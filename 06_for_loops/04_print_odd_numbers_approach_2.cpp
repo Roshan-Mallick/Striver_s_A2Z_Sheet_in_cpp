bb7f1a23231814dbb500fc40dd05edf8e9053977
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-14 00:11:02 +0530

    feat: print odd numbers using step increment in for loop

---
+++ 06_for_loops/04_print_odd_numbers_approach_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+  int num ;
+  cout << "Enter num : ";
+  cin >> num ;
+  
+  for ( int i = 1 ; i <= num ; i += 2){
+    cout << i << endl;
+  }
+
+
+  return 0;
+}
