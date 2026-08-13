ce21d4afbb8c801b204777159854cb9ce21289f7
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-14 00:04:07 +0530

    feat: add program to print odd numbers using modulo operator

---
+++ 06_for_loops/04_print_odd_numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main  () {
+
+int num ;
+cout << "Enter num : ";
+cin >> num;
+
+for (int i = 1 ;  i <= num ; i++) {
+
+
+     if (i % 2 != 0 ){
+        cout << i << endl;
+     }
+}
+
+
+  return 0;
+}
