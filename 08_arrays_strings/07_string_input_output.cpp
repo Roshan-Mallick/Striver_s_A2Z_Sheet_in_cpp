a2e94b17f5bc7b02a91e9a34326aef36c82afded
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-26 08:59:56 +0530

    feat: add basic string input and output program

---
+++ 08_arrays_strings/07_string_input_output.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main () {
+
+    string name;
+    cout << "Enter you name : ";
+    cin >> name;
+    
+    cout << "name is : "<< name << endl ;
+  
+
+
+ return 0 ;
+}
